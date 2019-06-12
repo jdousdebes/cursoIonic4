577196fcdb5f6b34bec270eb6a7fc6df83e25365
warlock <jdousdebes@gmail.com>
2019-06-12 18:59:58 -0300

    fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  host: 'localhost:8000',
-  // host: 'curso.hawk-srl.com',
-  api: 'http://localhost:8000/api/',
-  // api: 'https://curso.hawk-srl.com/api/',
+  // host: 'localhost:8000',
+  host: 'curso.hawk-srl.com',
+  // api: 'http://localhost:8000/api/',
+  api: 'https://curso.hawk-srl.com/api/',
 
   googleplus: {
     webClientId: '74371797188-k2gl3caksct5sa58scfq9uq0eo9p5ctt.apps.googleusercontent.com',
